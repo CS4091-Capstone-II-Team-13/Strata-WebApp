acef4138d2c17ab65870c14c17694057b06096cf
KyleN29 <kyle.noelker@gmail.com>
2026-02-25 20:43:07 -0600

    Replaced default landing page and created basic navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+
+import strataLogo from '../assets/STRATA_Logo.png'
+import './Navbar.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-logo-container">
+            <img src={strataLogo} className="logo" alt="React logo" />
+        </div>
+        <div className="navbar-login-container">
+            <div>
+                Login
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
