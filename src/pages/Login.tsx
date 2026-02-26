3e2cbbe267b216256e1d8e7506657f23b0758d3f
KyleN29 <kyle.noelker@gmail.com>
2026-02-25 21:13:52 -0600

    Add sign in form to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,33 @@
-
+import strataLogo from "../assets/STRATA_Logo.png";
 import "./Login.css";
 function Login() {
     return (
         <>
-        <div>
-            Login
-        </div>
+            <div className="login-container">
+                <div className="login">
+                    <div className="login-header">
+                        <img
+                            src={strataLogo}
+                            className="login-logo"
+                            alt="React logo"
+                        />
+                        <div>Sign in to STRATA</div>
+                    </div>
+                    <div className="login-entries">
+                        <div className="login-email-container">
+                            <div>Email</div>
+                            <input type="text" />
+                        </div>
+                        <div className="login-password-container">
+                            <div>Password</div>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div className="login-button">
+                        <button>Sign in</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
