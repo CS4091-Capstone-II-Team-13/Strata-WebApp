2f8301cb49797372eda381278f16a16f820a45e8
Kyle Noelker <gcarey0@outlook.com>
2026-03-23 00:41:58 -0500

    Connect login navbar button to login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router";
 import strataLogo from "../assets/STRATA_Logo.png";
 import "./Navbar.css";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="navbar">
@@ -9,7 +11,12 @@ function Navbar() {
           <img src={strataLogo} className="logo" alt="React logo" />
         </div>
         <div className="navbar-login-container">
-          <div>Login</div>
+          <div
+            className="navbar-login-button"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </div>
         </div>
       </div>
     </>
